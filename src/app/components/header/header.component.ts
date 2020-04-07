c430b498ed52b062e610660ec55ee02aad01d430
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-04-07 00:04:37 -0500

    componente header visualizado correctamente

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
+
 @Component({
     selector: 'app-header',
     template: `<h1>Header Component</h1>`
 })
-class HeaderComponent{
+export class HeaderComponent{
 
 }
